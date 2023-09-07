7c8a041368fbc51a6cd2ce5af70f0de1a031dd72
Hiltlairen <sirjhoswilliam5@gmail.com>
2023-09-07 11:30:54 -0400

    agregando archivos js

---
+++ index.js
@@ -0,0 +1,15 @@
+const shareDiv = document.createElement("div");
+const cantidad = document.createElement("span");
+const shares = document.createElement("span");
+
+const text = document.createTextNode("1k");
+const text2 = document.createTextNode("Shares");
+
+cantidad.appendChild(text);
+shares.appendChild(text2);
+
+shareDiv.appendChild(cantidad);
+shareDiv.appendChild(shares);
+
+const stat = document.querySelector(".stat");
+stat.appendChild(shareDiv);
